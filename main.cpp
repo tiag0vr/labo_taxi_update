4ec04b7a753e256c7d9128d151868e9eedf882e4
Tiago De Oliveira Jorge <tiago.deoliveirajorge@heig-vd.ch>
2022-10-16 22:40:56 +0200

    On a été sauvés

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ Compilateur : Mingw-w64 g++ 11.1.0
 #include <iostream>
 #include <limits>
 #include <iomanip>
+#include <cmath>
 
 // Ajout d'une macro pour vider le buffer
 // Tous les vidages de buffer ont été remplacés par la macro
@@ -30,14 +31,18 @@ int main() {
 
    // Déclaration des constantes
    // Ajout du caractère f devant les valeurs des variables float
-	const float PRISE_EN_CHARGE = 5.0f;
-	const float SURTAXE_PAR_BAGAGE = 2.5f;
-	const float TARIF_MIN_JOUR = 1.0f;        // Ajout de constante pour le tarif jour
-   const float TARIF_MIN_NUIT = 1.6f;        // Ajout de constante pour le tarif nuit
-	const int MIN_PAR_HEURE = 60;
-   const string HORAIRE_JOUR = "8h00-20h00"; // Ajout de constante pour l'horaire du tarif jour
-	const short P = 2;                        // Variable de précision
-	const short W = 26;                       // Variable de largeur d'indentation dans le terminal
+	const float PRISE_EN_CHARGE      = 5.0f;
+	const float SURTAXE_PAR_BAGAGE   = 2.5f;
+	const float TARIF_MIN_JOUR       = 1.0f;        // Ajout de constante pour le tarif jour
+   const float TARIF_MIN_NUIT       = 1.6f;        // Ajout de constante pour le tarif nuit
+	const int MIN_PAR_HEURE          = 60;
+   const string HORAIRE_JOUR        = "8h00-20h00"; // Ajout de constante pour l'horaire du tarif jour
+	const short P                    = 2;                        // Variable de précision
+	const short W                    = 26;                       // Variable de largeur d'indentation dans le terminal
+
+   const int FIN_HORAIRE_NUIT_MIN   = 480;
+   const int DEBUT_HORAIRE_NUIT_MIN = 1200;
+   const int HEURE_MINUIT_MIN       = 1440;
 
    // Suppression de l'initialisation des variables avec valeur 0
 	unsigned short nbreBagages;
@@ -66,9 +71,9 @@ int main() {
         << HORAIRE_JOUR << endl;
 
    // Ajout des limites des inputs utilisateur et ajout de l'heure de départ
-	cout << endl << "Votre commande" << endl
-		  << "================================" << endl
-		  << setw(W) << " - nbre de bagage              [0 - 4]" << ": ";
+   cout << endl << "Votre commande" << endl
+		  << "================================" << endl;
+		  cout << setw(W) << left << " - nbre de bagage [0 - 4]" << ": ";
 	     cin >> nbreBagages;
         CLEAR_BUF;
         // Ajout de la validation de l'entrée du nombre de bagages
@@ -76,8 +81,7 @@ int main() {
            cout << "Cette entree est invalide !" << endl;
            END;
         }
-
-		  cout << left << setw(W) <<  " - distance [km] [0 - 500]" << ": ";
+        cout << left << setw(W) <<  " - distance [km] [0 - 500]" << ": ";
 		  cin >> distanceParcours;
         CLEAR_BUF;
         // Ajout de la validation de l'entrée de la distance du parcours
@@ -86,7 +90,7 @@ int main() {
            END;
         }
 
-		  cout << left << setw(W) << " - vitesse  [km/h] [30-120] :";
+		  cout << left << setw(W) << " - vitesse  [km/h] [30-120]" << ": ";
 		  cin >> vitesseMoyenne;
         CLEAR_BUF;
         // Ajout de la validation de la vitesse moyenne
@@ -96,7 +100,7 @@ int main() {
         }
 
         char c; // Caractère pour insérer et ignorer ':'
-        cout << left << setw(W) << " - depart  [hh:mm] : ";
+        cout << left << setw(W) << " - depart   [hh:mm] : ";
         cin >> heureDepart >> c >> minutesDepart;
         CLEAR_BUF;
         // Ajout de la validation de l'heure de départ
@@ -106,17 +110,54 @@ int main() {
         }
 
 
-	// Calcul du supplément de bagages
-	float suppBagages = SURTAXE_PAR_BAGAGE * nbreBagages;
-
 	// Calcul du prix de la course
-   /// A CORRIGER
-	float prixCourse = distanceParcours / vitesseMoyenne * MIN_PAR_HEURE
-							 * TARIF_MIN_JOUR;
+   int dureeCourse      = ceil(distanceParcours * 60 / vitesseMoyenne);
+   int departMin        = heureDepart * 60 + minutesDepart;
+   int dureeCourseNuit  = 0;
+   int dureeCourseJour  = 0;
+   int tempCalc;  // Variable temporaire utilisée pour le calcul des courses entre horaire jour et nuit
 
-	// Calcul du prix total
-	float total = PRISE_EN_CHARGE + suppBagages + prixCourse;
+   while(dureeCourse > 0){
+      // Si le départ est dans l'horaire de nuit (0h00 - 7h59)
 
+      if(departMin < FIN_HORAIRE_NUIT_MIN){
+         // Test si la durée de la course nuit est continuée en horaire jour
+         departMin + dureeCourse < FIN_HORAIRE_NUIT_MIN ? tempCalc = dureeCourse : tempCalc = FIN_HORAIRE_NUIT_MIN -
+            departMin;
+         dureeCourseNuit   += tempCalc;
+         dureeCourseJour   -= tempCalc;
+         departMin     = FIN_HORAIRE_NUIT_MIN;
+      }
+
+      // Test si le départ est durant l'horaire de jour (8h00 - 19h59)
+      if(departMin < DEBUT_HORAIRE_NUIT_MIN){
+
+         departMin + dureeCourse < DEBUT_HORAIRE_NUIT_MIN ? tempCalc = dureeCourse : tempCalc =
+            DEBUT_HORAIRE_NUIT_MIN - departMin;
+
+         dureeCourseJour += tempCalc;
+         dureeCourse     -= tempCalc;
+         departMin       = DEBUT_HORAIRE_NUIT_MIN;
+      }
+
+      // Test si le départ est durant l'horaire de nuit (20h00 - 23h59)
+      if(departMin >= DEBUT_HORAIRE_NUIT_MIN){
+
+         departMin + dureeCourse < HEURE_MINUIT_MIN ? tempCalc = dureeCourse : tempCalc = HEURE_MINUIT_MIN - departMin;
+
+         dureeCourseNuit += tempCalc;
+         dureeCourse     -= tempCalc;
+         departMin        = 0;
+      }
+   }
+
+   // Calcul du prix total
+   float suppBagages       = SURTAXE_PAR_BAGAGE * nbreBagages;
+   float prixCourseJour    = dureeCourseJour * TARIF_MIN_JOUR;
+   float prixCourseNuit    = dureeCourseNuit * TARIF_MIN_NUIT;
+   float prixTotalCourse   = PRISE_EN_CHARGE + suppBagages + prixCourseJour + prixCourseNuit;
+
+   // Affichage du ticket
 	cout << fixed << setprecision(P);
 	cout << endl << "Votre ticket" << endl
 		  << "================================" << endl
